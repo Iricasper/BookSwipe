cf22759051a66d055bfd047387eb0a2012b19d11
Iris <acasper631@g.educaand.es>
2025-11-28 09:17:12 +0100

    ListScreen en proceso

--- app/src/main/java/com/aplimoviles/bookswipe/ui/screens/element/LibroListScreen.kt
+++ app/src/main/java/com/aplimoviles/bookswipe/ui/screens/element/LibroListScreen.kt
@@ -1,11 +1,16 @@
 package com.aplimoviles.bookswipe.ui.screens.element
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
+import com.aplimoviles.bookswipe.model.Libro
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 @Composable
 fun LibroListScreen(auth: FirebaseAuth, database: DatabaseReference, navController: NavController) {
@@ -13,6 +18,21 @@ fun LibroListScreen(auth: FirebaseAuth, database: DatabaseReference, navControll
     val user = auth.currentUser
 
     val librosRef = FirebaseDatabase.getInstance().getReference("libro")
-    database.child("libro")
+
+    librosRef.addValueEventListener(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val libros = snapshot.children.mapNotNull {
+                it.getValue(Libro::class.java)?.apply { isbn = it.key.toString() }
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Toast.makeText(context, "Error al cargar los libros", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+    )
 
 }
